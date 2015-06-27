0f06b3b5a6e77a0673be5222e7865fdb492deb55
Sam Stavinoha <smlstvnh@gmail.com>
2015-06-27 16:25:14 -0500

    :paperclip: package info

---
+++ requests_chef/__about__.py
@@ -0,0 +1,35 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Package attributes and metadata."""
+
+__all__ = (
+    '__title__',
+    '__summary__',
+    '__url__',
+    '__version__',
+    '__author__',
+    '__email__',
+    '__license__',
+    '__copyright__',
+    '__keywords__',
+)
+
+
+__title__ = 'requests-chef'
+__summary__ = 'Chef Authentication protocol support for Python-Requests'
+__url__ = 'https://github.com/samstav/requests-chef'
+__version__ = '0.0.1'
+__author__ = 'Sam Stavinoha'
+__email__ = 'samuel.stavinoha@rackspace.com'
+__keywords__ = ['opscode', 'chef', 'requests', 'authentication', 'auth']
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2015 Sam Stavinoha'
